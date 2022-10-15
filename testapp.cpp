d6aaf773ebb5d1e96b3e8964cad47946857b057c
VitaliyMinaev <bezuglivita123@gmail.com>
2022-10-15 04:48:02 -0700

    First commit

---
+++ testapp.cpp
@@ -0,0 +1,34 @@
+﻿#define _WIN32_DCOM
+
+#include <iostream>
+#include <WbemIdl.h>
+#pragma comment(lib, "wbemuuid.lib")
+
+using namespace std;
+
+int main() {
+	HRESULT hr;
+	hr = CoInitializeEx(0, COINITBASE_MULTITHREADED);
+	if (FAILED(hr) == true) {
+		cout << "Failed to initialize con library. Error code 0x" << hex << hr << endl;
+		return 1;
+	}
+	hr = CoInitializeSecurity(
+		NULL,
+		-1,
+		NULL,
+		NULL,
+		RPC_C_AUTHN_LEVEL_DEFAULT,
+		RPC_C_IMP_LEVEL_IMPERSONATE,
+		NULL,
+		EOAC_NONE,
+		NULL);
+
+	if (FAILED(hr) == true) {
+		cout << "Failed to initialize con library. Error code 0x" << hex << hr << endl;
+		return 1;
+	}
+
+
+	return 0;
+}
